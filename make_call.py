aba62625306dfbb2e50f1764a9de1c3ba4fdefde
JAATBHAIJI <47389600+JAATBHAIJI@users.noreply.github.com>
2021-10-25 10:08:04 +0530

    Update make_call.py

--- make_call.py
+++ make_call.py
@@ -7,7 +7,7 @@ client = Client(account_sid, auth_token)
 call = client.calls.create(
     to="+918433120646",
     from_="+12512921406",
-    url="file:///C:/Users/Nishant/OneDrive/Desktop/Work/gsm%20ckt/.wav%20audio/intro.mp3"
+    url="https://demo.twilio.com/docs/voice.xml"
 )
 
 print(call.sid)
